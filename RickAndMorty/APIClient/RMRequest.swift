dea9bf6d0b2b30376513220b7bf6965945f2ad7d
AlmazAtabaev <102241542+AlmazAtabaev@users.noreply.github.com>
2023-02-09 22:18:38 +0600

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,15 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Алмаз Атабаев on 9/2/23.
+//
+
+import Foundation
+
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
+
--- RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+/// Controller to show and search for Episode
 final class RMEpisodeViewController: UIViewController {
 
     override func viewDidLoad() {
